faa11bbaa8742a5d205aaad6c7cd54a4936b4c23
Imusinov <igor.musinov@phystech.edu>
2020-02-03 23:30:44 +0000

    added container  deletion

--- examples/mnist/aci/launch_aci.sh
+++ examples/mnist/aci/launch_aci.sh
@@ -6,4 +6,6 @@ envsubst < aci_spec.yaml > _aci_spec.yaml
 azcopy cp "_aci_spec.yaml" "https://$ACI_PERS_STORAGE_ACCOUNT_NAME.file.core.windows.net/$ACI_PERS_SHARE_NAME/$(whoami)/mnist/_aci_spec.yaml?$SAS_TOKEN"
 azcopy cp "$1.py" "https://$ACI_PERS_STORAGE_ACCOUNT_NAME.file.core.windows.net/$ACI_PERS_SHARE_NAME/$(whoami)/mnist/$1.py?$SAS_TOKEN"
 az container create -g $ACI_PERS_RESOURCE_GROUP --file _aci_spec.yaml
-azcopy "https://$ACI_PERS_STORAGE_ACCOUNT_NAME.file.core.windows.net/$ACI_PERS_SHARE_NAME/$(whoami)/mnist?$SAS_TOKEN" ./output --recursivly
+azcopy "https://$ACI_PERS_STORAGE_ACCOUNT_NAME.file.core.windows.net/$ACI_PERS_SHARE_NAME/$(whoami)/mnist?$SAS_TOKEN" ./output --recursively
+az container delete -g $ACI_PERS_RESOURCE_GROUP -n $USER-gpucontainergroup -y
+
