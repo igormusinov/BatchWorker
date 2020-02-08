a21eef6337330665bc6b1b4cfbb1b649ef610ecd
Igor <igor.musinov@phystech.edu>
2020-02-08 12:50:27 +0300

    run_aks + convert first

--- examples/mnist-cpu/aci/run_job.py
+++ examples/mnist-cpu/aci/run_job.py
@@ -57,29 +57,30 @@ def console_command(cmd: list, timeout: int = 10000, *args, **kwargs):
                                stderr=subprocess.STDOUT,
                                universal_newlines=True, *args, **kwargs)
 
+    log = ""
     start = datetime.datetime.now()
     while (datetime.datetime.now() - start).seconds < timeout:
         output = process.stdout.readline()
+        log += output
         print(output.strip())
         return_code = process.poll()
         if return_code is not None:
             print('RETURN CODE', return_code)
             # Process has finished, read rest of the output
             for output in process.stdout.readlines():
+                log += output
                 print(output.strip())
             break
+    return log
 
 
-def aci_run(notebook: Path, config: Path):
+def aci_run(script: Path, config: Path):
     try:
         console_command(["az", "account", "show"])
     except Exception as exp:
         logging.error(
             f"Launch /etc/azcreds/azlogin.sh or login to your Azure account `az login`")
         console_command(["/bin/bash", "-c", "/etc/azcreds/azlogin.sh"])
-
-    console_command(["jupyter", "nbconvert", "--to script", str(notebook)])
-    script = notebook.parent / notebook.name.replace("ipynb", "py")
 
     EXPERIMENT_FOLDER = gen_tag(USER_CLUSTER_NAME)
     os.environ["EXPERIMENT_FOLDER"] = EXPERIMENT_FOLDER
@@ -112,8 +113,48 @@ def aci_run(notebook: Path, config: Path):
     return 0
 
 
-def aks_run():
-    pass
+def aks_run(script: Path, config: Path):
+    try:
+        console_command(["az", "account", "show"])
+    except Exception as exp:
+        logging.error(
+            f"Launch /etc/azcreds/azlogin.sh or login to your Azure account `az login`")
+        console_command(["/bin/bash", "-c", "/etc/azcreds/azlogin.sh"])
+
+    EXPERIMENT_FOLDER = gen_tag(USER_CLUSTER_NAME)
+    os.environ["EXPERIMENT_FOLDER"] = EXPERIMENT_FOLDER
+    os.environ["RESOURCE_NAME"] = EXPERIMENT_FOLDER
+    _config, _config_spec = proc_config(config)
+    job_name = f"{_config_spec['name']}"
+
+    STORAGE_URI = "https://{}.file.core.windows.net/{}/{}/{}?{}".format(
+        ACI_PERS_STORAGE_ACCOUNT_NAME,
+        ACI_PERS_SHARE_NAME,
+        EXPERIMENT_FOLDER,
+        "{}",
+        SAS_TOKEN)
+    console_command(["azcopy", "cp", str(_config), STORAGE_URI.format(_config.name)])
+    console_command(["azcopy", "cp", str(script), STORAGE_URI.format(script.name)])
+
+    console_command(
+        ["kubectl", "--kubeconfig=/etc/azcreds/kubeconfig.users", "apply", "-f",
+         _config])
+    pod_name = console_command(
+        ["kubectl", "--kubeconfig=/etc/azcreds/kubeconfig.users", "get", "pods", "|"
+                                                                                 "grep",
+         job_name, "|", "awk", "'{print $1}'"])
+    console_command(
+        ["kubectl", "--kubeconfig=/etc/azcreds/kubeconfig.users", "logs", "pod",
+         pod_name, "-f"])
+
+    console_command(["azcopy", "cp",
+                     STORAGE_URI.format('*'), f"./{EXPERIMENT_FOLDER}",
+                     "--recursive=true"])
+    console_command(
+        ["kubectl", "--kubeconfig=/etc/azcreds/kubeconfig.users", "delete", "-f",
+         _config])
+
+    return 0
 
 
 def slurm_run():
@@ -126,17 +167,24 @@ def main():
                         required=True)
     parser.add_argument('--config', help='Specify config file', required=True)
     parser.add_argument('--cluster', help='Choose cluster', default="ACI")
+    parser.add_argument('--locac_data_folder', help='Specify your local data folder')
+    parser.add_argument('--azure_data_uri',
+                        help='Specify your data in your azure storage')
     args = parser.parse_args()
 
     check_file(args.notebook)
     check_file(args.config)
+
     notebook = Path(args.notebook)
     os.environ["PYSCRIPT"] = notebook.name.replace(".ipynb", "")
+    console_command(["jupyter", "nbconvert", "--to script", str(notebook)])
+    script = notebook.parent / notebook.name.replace("ipynb", "py")
 
     if args.cluster == "ACI":
-        aci_run(notebook, Path(args.config))
+        aci_run(script, Path(args.config))
     else:
         logging.error(f"Method {args.cluster} is not realised :(")
+        raise
         return 2
 
 
