07d40681b88afc77e93670a41995b04e49b92d46
Igor <igor.musinov@phystech.edu>
2020-02-21 11:53:39 +0300

    +setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="BatchWorker",
+    version="1.0.0",
+    author="Igor Musinov && Dmitry Maevskiy",
+    author_email="igor.musinov@phystech.edu",
+    description="Small set of scripts for ACI, Kubernetes and Slurm run",
+    url="https://github.com/igormusinov/BatchWorker",
+    package=setuptools.find_packages(),
+    python_requires='>=3.0'
+)
